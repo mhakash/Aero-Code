252c2e8a40fff141a6569e14769f6e3e11ff584b
mhakash <mhakash211998@gmail.com>
2021-05-28 01:45:19 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
