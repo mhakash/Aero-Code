0732a4af4561b925814f8304a4d0bcd9cb6446bc
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-06-01 20:39:50 +0600

    fix typo

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -7,7 +7,7 @@ export const createUser = async (user: User): Promise<User> => {
     email: user.email,
     avatar: user.avatar,
     profileCompleted: false,
-    codes:user.codes
+    codes: user.codes
   };
 
   try {
