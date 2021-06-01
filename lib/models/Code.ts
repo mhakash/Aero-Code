49355eb547c670a772f109b5fc98b42bd126776f
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-06-01 17:47:19 +0600

    add model

---
+++ lib/models/Code.ts
@@ -0,0 +1,32 @@
+import { dbConnect } from '../utils/dbConnect';
+import { Code } from 'types';
+
+export const createCode = async (user_id: string, name: string, reviewers?: string[]): Promise<Code> => {
+  const temp = {
+    user_id: user_id,
+    name: name,
+    reviewers: reviewers
+  };
+
+  try {
+    const codeCollection = (await dbConnect()).db.collection('codes');
+    const res = await codeCollection.insertOne(temp);
+    const code:Code = {
+        ... temp, _id: res.insertedId
+    }
+    return code;
+  } catch (err) {
+    throw new Error('Could not create user\n' + err?.message);
+  }
+};
+
+export const getCodeById = async (id: string): Promise<Code | null> => {
+  try {
+    const codeCollection = (await dbConnect()).db.collection('codes');
+    const result = await codeCollection.findOne({ _id: id });
+    
+    return result;
+  } catch (err) {
+    throw new Error('connection error');
+  }
+};
