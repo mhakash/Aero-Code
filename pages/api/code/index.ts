4dfa6fb2e5bcc66c0b5e235550c7876c90faf59d
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-06-01 20:28:28 +0600

    set filename as id

--- pages/api/code/index.ts
+++ pages/api/code/index.ts
@@ -2,6 +2,7 @@ import { createCode } from 'lib/models/Code';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { decodeToken } from '../../../lib/utils/firebaseAdmin';
 import { createPost } from '../../../lib/utils/objectStorage';
+import { Code } from 'types';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const token = req.headers['x-firebase-token'];
@@ -13,8 +14,8 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
       const file_name = (Array.isArray(req.query.file))? req.query.file[0] : req.query.file;
 
       // TODO: find out who are reviewers
-      await createCode(user_id, file_name);
-      const post = await createPost('code', file_name);
+      const code:Code = await createCode(user_id, file_name);
+      const post = await createPost('code', code._id);
       res.status(200).json(post);
     } catch (err) {
       res.status(404).json({ message: 'unauthorized' }); // TODO:
