6ec625e1c0e08d6b39a80faa3b2bc5e7afe0c3c5
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-06-01 19:47:15 +0600

    get code files

--- pages/api/raw/[pid].ts
+++ pages/api/raw/[pid].ts
@@ -9,9 +9,10 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
 
     // TODO: check authorization
     try {
-      await decodeToken(req.headers['x-firebase-token'] as string);
+      //await decodeToken(req.headers['x-firebase-token'] as string);
       const data = await getObject('code', key);
-      res.status(200).setHeader('Content-Type', 'image/jpeg').end(data, 'binary');
+      // res.status(200).setHeader('Content-Type', 'image/jpeg').end(data, 'binary');
+      res.status(200).send(data?.toString());
     } catch (err) {
       res.status(404).send('not found');
     }
