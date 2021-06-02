90a46fc92370d8b778260de1cf08b5bf1e8e63d4
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-06-02 11:25:00 +0600

    add friend to userdb

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -2,12 +2,15 @@ import { dbConnect } from '../utils/dbConnect';
 import { User } from 'types';
 
 export const createUser = async (user: User): Promise<User> => {
+
   const newUser = {
     _id: user._id,
     name: user.name,
     email: user.email,
     avatar: user.avatar,
     profileCompleted: false,
+    friends: [],
+    codes: []
   };
 
   try {
@@ -25,6 +28,21 @@ export const getUserById = async (id: string): Promise<User | null> => {
     const userCollection = (await dbConnect()).db.collection('users');
     const result = await userCollection.findOne({ _id: id });
 
+    return result;
+  } catch (err) {
+    throw new Error('connection error');
+  }
+};
+
+export const getUsersByName = async (name: string): Promise<User[]> => {
+  try {
+    const userCollection = (await dbConnect()).db.collection('users');
+    const cursor = await userCollection.find({ $text: { $search: name } });
+    const result:User[] = []
+    for await (const doc of cursor) {
+      result.push(doc);
+    }
+    console.log(result);
     return result;
   } catch (err) {
     throw new Error('connection error');
