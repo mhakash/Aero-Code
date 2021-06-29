673b9ddaacc742c85f9bce6a638d9a6dd021090a
mhakash <mhakash211998@gmail.com>
2021-06-29 17:35:35 +0600

    fix image host

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
   webpackDevMiddleware: (config) => {
     config.watchOptions = {
       poll: 1000, // Check for changes every second
