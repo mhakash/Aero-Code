e762f78c2d463e27dcb80a9293807c6cd49c02ab
habibshafin <53096312+habibshafin@users.noreply.github.com>
2021-06-30 13:39:12 +0600

    Message homepage (#16)
    * message-needs-final
    
    * messagesavesolve

--- pages/api/message/[chid].ts
+++ pages/api/message/[chid].ts
@@ -1,19 +1,31 @@
-import { getCodeById } from 'lib/models/Code';
-import { getMessageByChatRoomId } from 'lib/models/Message';
+import { getMessageByChatRoomId, addMessage } from 'lib/models/Message';
 import { decodeToken } from 'lib/utils/firebaseAdmin';
-import { getObject } from 'lib/utils/objectStorage';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const { chid } = req.query;
   const key = Array.isArray(chid) ? chid[0] : chid;
+  const token = req.headers['x-firebase-token'];
+  
   // TODO: check authorization
   try {
-    await decodeToken(req.headers['x-firebase-token'] as string);
+    if(req.method === 'GET'){
+      await decodeToken(req.headers['x-firebase-token'] as string);
     const msgs = await getMessageByChatRoomId(key);
     console.log(msgs);
     // res.status(200).setHeader('Content-Type', 'image/jpeg').end(data, 'binary');
     res.status(200).json(msgs);
+    }
+    else if (req.method === 'POST') {
+      try {
+          const t = await decodeToken(token as string);
+          const user_id = t.uid;
+          const res2 = await addMessage(req.body.chat_room_id,user_id,req.body.msg);
+          res.status(200).json(res2);
+      } catch (err) {
+        res.status(404).json({ message: 'unauthorized' }); // TODO:
+      }
+    }
   } catch (err) {
     res.status(404).send('not found');
   }
